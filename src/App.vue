76133f507f42781939d79b7e76ef98db2764427f
Kaleab Solomon <kaleabslmn@gmail.com>
2023-05-26 11:13:04 +0300

    completed the header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <main class="h-screen w-screen">
+    <div class="max-w-1000 p-2.5 my-0 mx-auto">
+      <header class="flex justify-between items-center">
+        <h1 class="font-bold mb-25 text-7xl">Notes</h1>
+        <button
+          class="border-none p-2.5 w-50 h-50 cursor-pointer rounded-full text-white bg-black text-xl"
+        >
+          +
+        </button>
+      </header>
+      <div>
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
 <style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        1000: "1000px",
+      },
+      margin: {
+        25: "25px",
+      },
+      width: {
+        50: "50px",
+      },
+      height: {
+        50: "50px",
+      },
+    },
   },
   plugins: [],
 };
