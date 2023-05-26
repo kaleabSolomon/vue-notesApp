a7519f6f43e9de4da5c33faf33b029e0e1da317a
Kaleab Solomon <kaleabslmn@gmail.com>
2023-05-26 16:56:03 +0300

    made the modal dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const showModal = ref(false);
+</script>
 
 <template>
   <div
+    v-if="showModal"
     class="absolute w-full h-full bg-black/80 z-10 flex items-center justify-center"
   >
     <div class="w-750 bg-white rounded-xl p-7 relative flex flex-col min-h-200">
@@ -14,6 +18,7 @@
         </button>
         <button
           class="py-2.5 px-5 bg-black text-white cursor-pointer mt-3 mx-auto w-1/3"
+          @click="showModal = false"
         >
           Close
         </button>
@@ -23,9 +28,10 @@
   <main class="h-screen w-screen">
     <div class="max-w-1000 p-2.5 my-0 mx-auto">
       <header class="flex justify-between items-center">
-        <h1 class="font-bold mb-25 text-7xl">Notes</h1>
+        <h1 class="font-bold mb-25 text-7xl">Notes {{ showModal }}</h1>
         <button
           class="border-none p-2.5 w-12 h-12 cursor-pointer rounded-full text-white bg-black text-xl"
+          @click="showModal = true"
         >
           +
         </button>
