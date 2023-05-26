427237e2e5819538dbbee5cfcb1db611a9808c2f
Kaleab Solomon <kaleabslmn@gmail.com>
2023-05-26 16:16:00 +0300

    created and styled the modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup></script>
 
 <template>
+  <div
+    class="absolute w-full h-full bg-black/80 z-10 flex items-center justify-center"
+  >
+    <div class="w-750 bg-white rounded-xl p-7 relative flex flex-col min-h-200">
+      <textarea name="notes" id="notes" cols="30" rows="10"></textarea>
+      <div class="flex w-1/2 m-auto">
+        <button
+          class="py-2.5 px-5 bg-cyan-500 text-white cursor-pointer mt-3 mx-auto w-1/3"
+        >
+          Add Note
+        </button>
+        <button
+          class="py-2.5 px-5 bg-black text-white cursor-pointer mt-3 mx-auto w-1/3"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
   <main class="h-screen w-screen">
     <div class="max-w-1000 p-2.5 my-0 mx-auto">
       <header class="flex justify-between items-center">
         <h1 class="font-bold mb-25 text-7xl">Notes</h1>
         <button
-          class="border-none p-2.5 w-50 h-50 cursor-pointer rounded-full text-white bg-black text-xl"
+          class="border-none p-2.5 w-12 h-12 cursor-pointer rounded-full text-white bg-black text-xl"
         >
           +
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ export default {
       maxWidth: {
         1000: "1000px",
       },
+      width: {
+        750: "750px",
+      },
+      minHeight: {
+        200: "200px",
+      },
       margin: {
         25: "25px",
-      },
-      width: {
-        50: "50px",
-      },
-      height: {
-        50: "50px",
       },
     },
   },
